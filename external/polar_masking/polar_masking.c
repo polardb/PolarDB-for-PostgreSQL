835869668982624b340bff11d30ecdbb8bacbe52
Muyijia <yang.jia3@zte.com.cn>
2026-07-13 10:06:42 +0800

    feat: data masking (masking operator node)
    This commit provides function of polar masking:
    - transform Var node to masking function Node

--- external/polar_masking/polar_masking.c
+++ external/polar_masking/polar_masking.c
@@ -14,6 +14,7 @@
 
 #include "nodes/makefuncs.h"
 #include "rewrite/rewriteHandler.h"
+#include "utils/fmgroids.h"
 #include "utils/guc.h"
 #include "utils/lsyscache.h"
 
@@ -29,9 +30,11 @@ static inline Node *create_string_node(Datum dat);
 static inline Node *create_integer_node(int value);
 static inline Node *create_relabeltype_node(Node *node, int resulttype);
 static void polar_process_masking_after_rewrite(List *query_list);
-
-Node	   *make_func_node(int funcid, Oid rettype, Node *arg);
-Node	   *make_func_node_regex(int funcid, Oid rettype, Node *arg, MaskingInfo * maskinfo);
+static Node *make_func_node(int funcid, Oid rettype, Node *arg);
+static Node *make_func_node_regex(int funcid, Oid rettype, Node *arg, MaskingInfo * maskinfo);
+static Node *create_masking_func_node(Var *var, int masking_op);
+static Node *create_masking_func_node_regex(Var *var, MaskingInfo * maskinfo);
+Node	   *create_masking_var_node(MaskingInfo * maskinfo, Var *var);
 
 /*
  * function for creating string node for masking function nodes' arg
@@ -97,7 +100,7 @@ create_relabeltype_node(Node *node, int resulttype)
 /*
  * function for creating masking function node
  */
-Node *
+static Node *
 make_func_node(int funcid, Oid rettype, Node *arg)
 {
 	FuncExpr   *funcexpr = makeNode(FuncExpr);
@@ -121,7 +124,7 @@ make_func_node(int funcid, Oid rettype, Node *arg)
 /*
  * function for creating regexp masking function node
  */
-Node *
+static Node *
 make_func_node_regex(int funcid, Oid rettype, Node *arg, MaskingInfo * maskinfo)
 {
 	FuncExpr   *funcexpr = makeNode(FuncExpr);
@@ -145,6 +148,181 @@ make_func_node_regex(int funcid, Oid rettype, Node *arg, MaskingInfo * maskinfo)
 
 	return (Node *) funcexpr;
 }
+
+/*
+ * function for transforming Var node to masking Var node
+ */
+static Node *
+create_masking_func_node(Var *var, int masking_op)
+{
+	Node	   *masking_func_node = NULL;
+	Oid			funcid = get_masking_funcid(masking_op);
+
+	if (funcid == InvalidOid)
+		return masking_func_node;
+
+	/*
+	 * create masking function node with var according to var type, masking
+	 * func nodes have to be casted to the original type of Var nodes
+	 */
+	switch (var->vartype)
+	{
+		case TEXTOID:
+			{
+				masking_func_node = make_func_node(funcid, TEXTOID, (Node *) var);
+			}
+			break;
+		case CHAROID:
+		case BPCHAROID:
+		case VARCHAROID:
+			{
+				masking_func_node = make_func_node(funcid, TEXTOID, (Node *) var);
+				if (masking_func_node != NULL)
+				{
+					Node	   *cast_node = create_relabeltype_node(masking_func_node, var->vartype);
+
+					if (cast_node != NULL)
+					{
+						masking_func_node = cast_node;
+					}
+				}
+			}
+			break;
+		case NAMEOID:
+			{
+				Node	   *text_node = make_func_node(F_NAME_TEXT, TEXTOID, (Node *) var);
+
+				if (text_node != NULL)
+					masking_func_node = make_func_node(funcid, TEXTOID, text_node);
+				if (masking_func_node != NULL)
+				{
+					Node	   *cast_func = make_func_node(F_TEXT_NAME, NAMEOID, masking_func_node);
+
+					if (cast_func != NULL)
+					{
+						masking_func_node = cast_func;
+					}
+				}
+			}
+			break;
+		default:
+			break;
+	}
+	return masking_func_node;
+}
+
+/*
+ * function for transforming Var node to regex masking Var node
+ */
+static Node *
+create_masking_func_node_regex(Var *var, MaskingInfo * maskinfo)
+{
+	Node	   *masking_func_node = NULL;
+	Oid			funcid = get_masking_funcid(maskinfo->masking_op);
+
+	if (funcid == InvalidOid)
+		return masking_func_node;
+
+	if (var == NULL)
+	{
+		return NULL;
+	}
+
+	/*
+	 * create masking function node with var according to var type, masking
+	 * func nodes have to be casted to the original type of Var nodes
+	 */
+	switch (var->vartype)
+	{
+		case TEXTOID:
+			{
+				masking_func_node = make_func_node_regex(funcid, TEXTOID, (Node *) var, maskinfo);
+			}
+			break;
+		case CHAROID:
+		case BPCHAROID:
+		case VARCHAROID:
+			{
+				masking_func_node = make_func_node_regex(funcid, TEXTOID, (Node *) var, maskinfo);
+				if (masking_func_node != NULL)
+				{
+					Node	   *cast_node = create_relabeltype_node(masking_func_node, var->vartype);
+
+					if (cast_node != NULL)
+					{
+						masking_func_node = cast_node;
+					}
+				}
+			}
+			break;
+		case NAMEOID:
+			{
+				Node	   *text_node = make_func_node(F_NAME_TEXT, TEXTOID, (Node *) var);
+
+				masking_func_node = make_func_node_regex(funcid, TEXTOID, text_node, maskinfo);
+				if (masking_func_node != NULL)
+				{
+					Node	   *cast_func = make_func_node(F_TEXT_NAME, NAMEOID, masking_func_node);
+
+					if (cast_func != NULL)
+					{
+						masking_func_node = cast_func;
+					}
+				}
+			}
+			break;
+		default:
+			break;
+	}
+	return masking_func_node;
+}
+
+/*
+ * function for create masking Var node according to masking operator
+ */
+Node *
+create_masking_var_node(MaskingInfo * maskinfo, Var *var)
+{
+	Node	   *masked_node = NULL;
+
+	switch (maskinfo->masking_op)
+	{
+		case MASKING_CREDITCARD:
+		case MASKING_BASICEMAIL:
+		case MASKING_FULLEMAIL:
+		case MASKING_SHUFFLE:
+		case MASKING_ALLDIGITS:
+		case MASKING_RANDOM:
+		case MASKING_ALL:
+			{
+				masked_node = create_masking_func_node(var, maskinfo->masking_op);
+			}
+			break;
+		case MASKING_REGEXP:
+			{
+				masked_node = create_masking_func_node_regex(var, maskinfo);
+			}
+			break;
+		default:
+			{
+				/*
+				 * should not happen
+				 */
+				elog(ERROR, "unknown masking operator: %d", maskinfo->masking_op);
+			}
+			break;
+	}
+
+	/*
+	 * Var node is not masked, return the original Var
+	 */
+	if (masked_node == NULL)
+	{
+		masked_node = (Node *) var;
+	}
+	return masked_node;
+}
+
 
 static void
 polar_process_masking_after_rewrite(List *query_list)
