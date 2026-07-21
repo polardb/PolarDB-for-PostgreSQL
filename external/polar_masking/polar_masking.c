b3b3f14a157e6763e1b26d87b30953db6b0eb2f8
Muyijia <yang.jia3@zte.com.cn>
2026-07-21 14:28:41 +0800

    feat: data masking (masking process)
    This commit provides function of polar masking:
    - process masking for simple select

--- external/polar_masking/polar_masking.c
+++ external/polar_masking/polar_masking.c
@@ -13,6 +13,8 @@
 #include "polar_masking.h"
 
 #include "nodes/makefuncs.h"
+#include "nodes/nodeFuncs.h"
+#include "nodes/parsenodes.h"
 #include "rewrite/rewriteHandler.h"
 #include "utils/fmgroids.h"
 #include "utils/guc.h"
@@ -34,7 +36,14 @@ static Node *make_func_node(int funcid, Oid rettype, Node *arg);
 static Node *make_func_node_regex(int funcid, Oid rettype, Node *arg, MaskingInfo * maskinfo);
 static Node *create_masking_func_node(Var *var, int masking_op);
 static Node *create_masking_func_node_regex(Var *var, MaskingInfo * maskinfo);
-Node	   *create_masking_var_node(MaskingInfo * maskinfo, Var *var);
+static Node *create_masking_var_node(MaskingInfo * maskinfo, Var *var);
+static void get_masking_relid_colid(List *rtable, Var *var, Oid *relid, AttrNumber *colid);
+static Node *masking_process_var_node(Node *src_var, List *rtable);
+static Node *masking_vars_walker(Node *node, void *rtable);
+static void masking_process_targetlist(List **targetList, List *rtable);
+static void masking_process_select_query(Query *query);
+static void masking_process_selectcmd(Query *query);
+static void masking_process_after_rewrite(Query *query);
 
 /*
  * function for creating string node for masking function nodes' arg
@@ -190,13 +199,13 @@ create_masking_func_node(Var *var, int masking_op)
 			break;
 		case NAMEOID:
 			{
-				Node	   *text_node = make_func_node(F_NAME_TEXT, TEXTOID, (Node *) var);
+				Node	   *text_node = make_func_node(F_TEXT_NAME, TEXTOID, (Node *) var);
 
 				if (text_node != NULL)
 					masking_func_node = make_func_node(funcid, TEXTOID, text_node);
 				if (masking_func_node != NULL)
 				{
-					Node	   *cast_func = make_func_node(F_TEXT_NAME, NAMEOID, masking_func_node);
+					Node	   *cast_func = make_func_node(F_NAME_TEXT, NAMEOID, masking_func_node);
 
 					if (cast_func != NULL)
 					{
@@ -257,12 +266,12 @@ create_masking_func_node_regex(Var *var, MaskingInfo * maskinfo)
 			break;
 		case NAMEOID:
 			{
-				Node	   *text_node = make_func_node(F_NAME_TEXT, TEXTOID, (Node *) var);
+				Node	   *text_node = make_func_node(F_TEXT_NAME, TEXTOID, (Node *) var);
 
 				masking_func_node = make_func_node_regex(funcid, TEXTOID, text_node, maskinfo);
 				if (masking_func_node != NULL)
 				{
-					Node	   *cast_func = make_func_node(F_TEXT_NAME, NAMEOID, masking_func_node);
+					Node	   *cast_func = make_func_node(F_NAME_TEXT, NAMEOID, masking_func_node);
 
 					if (cast_func != NULL)
 					{
@@ -323,14 +332,166 @@ create_masking_var_node(MaskingInfo * maskinfo, Var *var)
 	return masked_node;
 }
 
+/*
+ * function for getting the masking column's relid and colid
+ */
+static void
+get_masking_relid_colid(List *rtable, Var *var, Oid *relid, AttrNumber *colid)
+{
+	ListCell   *cell = NULL;
+	int			pos = 1;
+	RangeTblEntry *rte;
+
+	/*
+	 * find the rte for var
+	 */
+	foreach(cell, rtable)
+	{
+		if (pos != (int) var->varno)
+		{
+			++pos;
+			continue;
+		}
+
+		rte = (RangeTblEntry *) lfirst(cell);
+
+		/*
+		 * process normal relation only
+		 */
+		if (rte->rtekind == RTE_RELATION && rte->relkind == 'r')
+		{
+			*relid = rte->relid;
+			*colid = var->varattno;
+		}
+
+		break;
+	}
+}
+
+/*
+ * function for checking and processing masking for column
+ */
+static Node *
+masking_process_var_node(Node *src_var, List *rtable)
+{
+	Var		   *var;
+	MaskingInfo maskinfo;
+
+	MemSet(&maskinfo, 0, sizeof(MaskingInfo));
+
+	var = (Var *) src_var;
+
+	get_masking_relid_colid(rtable, var, &maskinfo.relid, &maskinfo.attnum);
+
+	if (check_masking_for_column(&maskinfo))
+	{
+		return create_masking_var_node(&maskinfo, var);
+	}
+	return src_var;
+}
+
+/*
+ * recurse process nodes in targetlist
+ */
+static Node *
+masking_vars_walker(Node *node, void *rtable)
+{
+	if (node == NULL)
+		return false;
+	if (IsA(node, Var))
+	{
+		Node	   *newnode = expression_tree_mutator(
+													  node, masking_vars_walker, (void *) rtable);
+
+		return masking_process_var_node(newnode, (List *) rtable);
+	}
+	if (IsA(node, Query))
+	{
+		masking_process_after_rewrite((Query *) node);
+		return node;
+	}
+	return expression_tree_mutator(
+								   node, masking_vars_walker, (void *) rtable);
+}
+
+/*
+ * process masking for targetlist
+ */
+static void
+masking_process_targetlist(List **targetList, List *rtable)
+{
+	List	   *target = *targetList;
+	Node	   *res;
+
+	if (target == NIL || rtable == NIL)
+	{
+		return;
+	}
+
+	res = masking_vars_walker((Node *) target, (void *) rtable);
+	list_free_deep(target);
+	*targetList = (List *) res;
+}
+
+static void
+masking_process_select_query(Query *query)
+{
+	Assert(query != NULL);
+	/* we only process vars in targetlist */
+	if (query->targetList != NIL)
+	{
+		masking_process_targetlist(&(query->targetList), query->rtable);
+	}
+}
+
+/*
+ * process masking for select
+ */
+static void
+masking_process_selectcmd(Query *query)
+{
+	if (query == NULL)
+	{
+		return;
+	}
+
+	masking_process_select_query(query);
+}
+
+/*
+ * process masking according to the query's command type.
+ */
+static void
+masking_process_after_rewrite(Query *query)
+{
+	switch (query->commandType)
+	{
+		case CMD_SELECT:
+			{
+				masking_process_selectcmd(query);
+				break;
+			}
+		default:
+			break;
+	}
+}
 
 static void
 polar_process_masking_after_rewrite(List *query_list)
 {
+	ListCell   *lc;
+	Node	   *node;
 
 	if (!polar_masking_enabled || query_list == NIL)
 	{
 		return;
+	}
+
+	foreach(lc, query_list)
+	{
+		node = (Node *) lfirst(lc);
+		if (nodeTag(node) == T_Query)
+			masking_process_after_rewrite((Query *) node);
 	}
 
 	if (next_polar_post_rewrite_query_hook)
